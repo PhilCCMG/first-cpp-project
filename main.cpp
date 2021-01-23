ca630e53ac2ecf644776413f02b9bac85d68cf05
Phil <p.gibson2003@gmail.com>
2021-01-23 00:44:06 +0000

    feat: bot says pong when you say ping
    i think. the code doesn't build because i haven't worked out how dependency management works.
    pass token as parameter to the application executable

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "sleepy_discord/sleepy_discord.h"
+
+class DiscordBotClient : public SleepyDiscord::DiscordClient {
+public:
+    using SleepyDiscord::DiscordClient::DiscordClient;
+    void onMessage(SleepyDiscord::Message message) override {
+        if (message.startsWith("ping")) {
+            sendMessage(message.channelID, "pong");
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    DiscordBotClient client(argv[0], SleepyDiscord::USER_CONTROLED_THREADS);
+    client.run();
+}
