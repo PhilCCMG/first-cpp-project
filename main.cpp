7ecbb37576f7c2395121e4a204720898b8547781
Phil <p.gibson2003@gmail.com>
2021-01-23 00:45:14 +0000

    fix: remove unused dependency

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include "sleepy_discord/sleepy_discord.h"
 
 class DiscordBotClient : public SleepyDiscord::DiscordClient {
 public:
     using SleepyDiscord::DiscordClient::DiscordClient;
+
     void onMessage(SleepyDiscord::Message message) override {
         if (message.startsWith("ping")) {
             sendMessage(message.channelID, "pong");
@@ -11,7 +11,7 @@ public:
     }
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     DiscordBotClient client(argv[0], SleepyDiscord::USER_CONTROLED_THREADS);
     client.run();
 }
